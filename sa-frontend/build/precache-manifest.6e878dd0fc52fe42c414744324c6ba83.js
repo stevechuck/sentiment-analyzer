ffbf6b548b3f1cf4900264762e4a4e2d5571798a
iamchuckss <s2011297@gmail.com>
2020-01-05 12:43:08 -0600

    add build for frontend

---
+++ sa-frontend/build/precache-manifest.6e878dd0fc52fe42c414744324c6ba83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ea847c2e03671a640dc972fd0182fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df154160abec25c99ce2",
+    "url": "/static/css/main.f7885002.chunk.css"
+  },
+  {
+    "revision": "b207c6719ad5074f7a14",
+    "url": "/static/js/2.a6a5e4c1.chunk.js"
+  },
+  {
+    "revision": "a19bf89d6eddc1c673db85a5ccd8da85",
+    "url": "/static/js/2.a6a5e4c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df154160abec25c99ce2",
+    "url": "/static/js/main.4b3d1e86.chunk.js"
+  },
+  {
+    "revision": "0c842f0211d3eace10c6",
+    "url": "/static/js/runtime-main.0a08162a.js"
+  }
+]);
